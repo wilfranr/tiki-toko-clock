6a66545a167aa5f223bea8799cfbcca15cef036b
wilfranr <wilfranr@gmail.com>
2025-08-01 15:12:15 -0500

    Initial commit: Add PWA files

---
+++ sw.js
@@ -0,0 +1,30 @@
+
+const CACHE_NAME = 'clock-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/images/icons/icon-192x192.png',
+  '/images/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
